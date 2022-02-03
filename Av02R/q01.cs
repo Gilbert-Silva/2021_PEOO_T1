42a6f41935d03668f69cd8b981d4edaa0454d1fc
GilbertAzevedo <gilbertif1995@outlook.com>
2022-02-03 19:46:26 +0000

    Avaliação 02

---
+++ Av02R/q01.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Program {
+  public static void Main() {
+    Console.Write("Informe o número de contas: ");
+    int n = int.Parse(Console.ReadLine());
+    Energia[] vetor = new Energia[n];
+    for (int i = 0; i < n; i++) {
+      Console.Write("Informe o mês/ano: ");
+      string ma = Console.ReadLine();
+      Console.Write("Informe o consumo em kWh: ");
+      int c = int.Parse(Console.ReadLine());
+      Console.Write("Informe o valor em R$: ");
+      double v = double.Parse(Console.ReadLine());
+      Energia e = new Energia(ma, c, v);
+      vetor[i] = e;
+    }
+    int totalc = 0;
+    double totalg = 0;
+    foreach(Energia e in vetor) {
+      totalc = totalc + e.GetConsumo();
+      totalg = totalg + e.GetValor();
+    }
+    Console.WriteLine($"Consumo total em kWh - {totalc}");
+    Console.WriteLine($"Total gasto em R$ - {totalg:0.00}");
+  }
+}
+
+class Energia {
+  private string mesAno;
+  private int consumo;
+  private double valor;
+  public Energia(string ma, int c, double v) {
+    this.mesAno = ma;
+    this.consumo = c;
+    this.valor = v;
+  }
+  public string GetMesAno() {
+    return mesAno;
+  }
+  public int GetConsumo() {
+    return consumo;
+  }
+  public double GetValor() {
+    return valor;
+  }
+}
---
+++ Av02R/q02.cs
@@ -0,0 +1,62 @@
+using System;
+
+class Program {
+  public static void Main() {
+    Jogador j1 = new Jogador("J1", 1, true);
+    Jogador j2 = new Jogador("J2", 2, false);
+    Jogador j3 = new Jogador("J3", 3, true);
+    //Console.WriteLine(j1);
+    //Console.WriteLine(j2);
+    //Console.WriteLine(j3);
+    Time t = new Time("Time");
+    t.Inserir(j1);
+    t.Inserir(j2);
+    t.Inserir(j3);
+    foreach(Jogador j in t.Listar())
+      Console.WriteLine(j);
+    Console.WriteLine(t.Capitao());  
+  }
+}
+
+class Time {
+  private string nome;
+  private Jogador[] vetor = new Jogador[11];
+  private int k;
+  public Time(string n) {
+    this.nome = n;
+  }
+  public void Inserir(Jogador j) {
+    //if (Capitao() != null && j.GetCapitao()) return;
+    if (Capitao() == null || j.GetCapitao()==false)      if (k < 11) {
+        vetor[k] = j;
+        k++;
+      }
+  }
+  public Jogador[] Listar() {
+    Jogador[] aux = new Jogador[k];
+    Array.Copy(vetor, aux, k);
+    return aux;
+  }
+  public Jogador Capitao() {
+    foreach(Jogador j in vetor)
+      if (j != null && j.GetCapitao()) return j;
+    return null;      
+  }
+}
+
+class Jogador {
+  private string nome;
+  private int camisa;
+  private bool capitao;
+  public Jogador(string n, int c, bool x) {
+    this.nome = n;
+    this.camisa = c;
+    this.capitao = x;
+  }
+  public bool GetCapitao() {
+    return capitao;
+  }
+  public override string ToString() {
+    return $"{nome} - {camisa} - {capitao}";
+  }
+}
